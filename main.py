7605d9411b0314184b2d5bba236c4b457985539f
Rolly Maulana Awangga <rolly@awang.ga>
2015-08-02 19:32:22 +0700

    add follower

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
+#print api.rate_limit_status()
+timelineonhome = api.home_timeline()
+api.send_direct_message(user = 'awangga',text = 'halo testing sending dm dari api')
+api.send_direct_message(user = 'awangga',text = 'halo testing sending dm dari api')
+for tweet in timelineonhome:
 	print tweet.text
+
+for follower in tweepy.Cursor(api.followers).items():
+	follower.follow()
+	
 	
 #print api.followers()
 #print api.direct_messages()
