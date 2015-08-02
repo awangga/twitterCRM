a41fc15c6ee029ee4936ef15271d45dd4842761b
Rolly Maulana Awangga <rolly@awang.ga>
2015-08-02 20:19:56 +0700

    follow follower and send message to them

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
 
 #print api.rate_limit_status()
-timelineonhome = api.home_timeline()
-api.send_direct_message(user = 'awangga',text = 'halo testing sending dm dari api')
-api.send_direct_message(user = 'awangga',text = 'halo testing sending dm dari api')
-for tweet in timelineonhome:
-	print tweet.text
-
+#timelineonhome = api.home_timeline()
+#api.send_direct_message(user = 'awangga',text = 'halo testing sending dm dari api')
+#api.send_direct_message(user = 'awangga',text = 'halo testing sending dm dari api')
+#for tweet in timelineonhome:
+#	print tweet.text
+#follow back follower
 for follower in tweepy.Cursor(api.followers).items():
 	follower.follow()
-	
+	print follower.screen_name
+	api.send_direct_message(user = follower.screen_name,text = config.message)
+#get list of follower
+#for follower in tweepy.Cursor(api.followers).items():
+#	print follower.screen_name
 	
 #print api.followers()
 #print api.direct_messages()
